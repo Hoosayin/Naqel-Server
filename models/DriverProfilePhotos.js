16dbc6e7ea2236a8531497849b6dd0c6b777a41e
Hussain Naeem <husain.sims@gmail.com>
2020-01-30 02:11:37 +0500

    Modified "DriverProfilePhotos" model.

---
+++ models/DriverProfilePhotos.js
@@ -0,0 +1,43 @@
+var databaseHelper = require('../helpers/databaseHelper');
+
+module.exports = databaseHelper.sequelize.define(
+    "DriverProfilePhotos",
+    {
+        ProfilePhotoID:
+        {
+            type: databaseHelper.Sequelize.BIGINT,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        DriverID:
+        {
+            type: databaseHelper.Sequelize.BIGINT,
+            allowNull: false,
+            references:
+            {
+                model: "Drivers",
+                key: "DriverID"
+            }
+        },
+        URL:
+        {
+            type: databaseHelper.Sequelize.TEXT,
+            allowNull: false
+        },
+        DateUploaded:
+        {
+            type: databaseHelper.Sequelize.DATEONLY,
+            defaultValue: databaseHelper.Sequelize.NOW,
+            allowNull: false
+        },
+        FileName:
+        {
+            type: databaseHelper.Sequelize.TEXT,
+            allowNull: false
+        },
+    },
+    {
+        tableName: "DriverProfilePhotos"
+    }    
+);
