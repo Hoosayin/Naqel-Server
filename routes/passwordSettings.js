4ab3da9f1765bdfd91df4161d7748aa79b876b15
Hussain Naeem <husain.sims@gmail.com>
2020-01-30 23:16:23 +0500

    Fixed errors in /passwordSettings.

--- routes/passwordSettings.js
+++ routes/passwordSettings.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const passport = require("../helpers/passportHelper");
 const Drivers = require("../models/drivers");
 const bcrypt = require("bcrypt");
 
@@ -10,32 +9,29 @@ var router = express.Router();
 router.use(cors());
 
 // POST: passwordSettings
-router.post("/dashboard/passwordSettings", (req, res, next) => {
+router.post("/dashboard/passwordSettings", (req, res) => {
     try {
         Drivers.findOne({
             where: { DriverID: req.body.DriverID },
-        })
-            .then(driver => {
-                if (!driver) {
-                    res.send("Driver not found.");
-                }
-                else {
-                    bcrypt.hash(req.body.Password, BCRYPT_SALT_ROUNDS)
-                        .then(passwordHash => {
-                            let updatedDriver = {
-                                Password: passwordHash,
-                            }
+        }).then(driver => {
+            if (!driver) {
+                res.send("Driver not found.");
+            }
+            else {
+                bcrypt.hash(req.body.Password, BCRYPT_SALT_ROUNDS).then(passwordHash => {
+                    let updatedDriver = {
+                        Password: passwordHash,
+                    }
 
-                            Drivers.update(updatedDriver, { where: { DriverID: req.body.DriverID } })
-                                .then(() => {
-                                    console.log("Driver is updated in database.");
-                                    res.send("Driver is updated.");
-                                });
-                        });
-                }
-            });
+                    Drivers.update(updatedDriver, { where: { DriverID: req.body.DriverID } }).then(() => {
+                        console.log("Driver is updated in database.");
+                        res.send("Driver is updated.");
+                    });
+                });
+            }
+        });
     } catch (error) {
-        return done(error);
+        return res.send(error);
     }
 });
 
