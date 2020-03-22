f2ab78129dc7a377f450b481a2d99f7cd90a28b7
Hussain Naeem <husain.sims@gmail.com>
2020-03-22 16:49:44 +0500

    Updated "passwordSettings" route for Traders | Brokers.

--- routes/traders/settings/passwordSettings.js
+++ routes/traders/settings/passwordSettings.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const bcrypt = require("bcrypt");
+const passport = require("../../../helpers/passportHelper");
 const Traders = require("../../../models/traders");
 
 const BCRYPT_SALT_ROUNDS = 12;
@@ -9,30 +10,33 @@ var router = express.Router();
 router.use(cors());
 
 // POST: passwordSettings
-router.post("/passwordSettings", (req, res) => {
-    try {
-        Traders.findOne({
-            where: { TraderID: req.body.TraderID },
-        }).then(trader => {
-            if (!trader) {
-                res.send("Trader not found.");
-            }
-            else {
-                bcrypt.hash(req.body.Password, BCRYPT_SALT_ROUNDS).then(passwordHash => {
+router.post("/passwordSettings", (request, response) => {
+    passport.authenticate("AuthenticateTrader", { session: false }, (result) => {
+        try {
+            if (result.Message === "Trader found.") {
+                bcrypt.hash(request.body.Password, BCRYPT_SALT_ROUNDS).then(passwordHash => {
                     let updatedTrader = {
                         Password: passwordHash,
-                    }
+                    };
 
-                    Traders.update(updatedTrader, { where: { TraderID: req.body.TraderID } }).then(() => {
-                        console.log("Trader/Broker is updated in database.");
-                        res.send("Trader/Broker is updated.");
+                    Traders.update(updatedTrader, { where: { TraderID: result.Trader.TraderID } }).then(() => {
+                        response.json({
+                            Message: "Trader is updated."
+                        });
                     });
                 });
             }
-        });
-    } catch (error) {
-        return done(error);
-    }
+            else {
+                response.json({
+                    Message: "Trader not found."
+                });
+            }
+        } catch (error) {
+            response.json({
+                Message: result.Message,
+            });
+        }
+    })(request, response);
 });
 
 module.exports = router;
