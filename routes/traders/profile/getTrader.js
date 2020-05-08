749df70dbfbf3b019eea9de89bed09e13a29b3af
Hussain Naeem <husain.sims@gmail.com>
2020-05-08 18:44:23 +0500

    Added "PhotoURL" property in "Trader" object.

--- routes/traders/profile/getTrader.js
+++ routes/traders/profile/getTrader.js
@@ -1,18 +1,26 @@
 const express = require("express");
 const cors = require("cors");
 const passport = require("../../../helpers/passportHelper");
+const TraderProfilePhotos = require("../../../models/traderProfilePhotos");
 
 var router = express.Router();
 router.use(cors());
 
 // GET: getTrader
 router.get("/getTrader", (request, response) => {
-    passport.authenticate("AuthenticateTrader", { session: false }, (result) => {
+    passport.authenticate("AuthenticateTrader", { session: false }, async result => {
         try {
             if (result.Message === "Trader found.") {
+                const profilePhoto = await TraderProfilePhotos.findOne({
+                    where: { TraderID: result.Trader.TraderID }
+                });
+
+                let trader = result.Trader.dataValues;
+                trader.PhotoURL = profilePhoto ? profilePhoto.PhotoURL : null;
+
                 response.json({
                     Message: "Trader found.",
-                    Trader: result.Trader
+                    Trader: trader
                 });
             }
             else {
