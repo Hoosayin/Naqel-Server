99b92d1ea174bc998bbdd3123c0ecc8d796fd219
Hussain Naeem <husain.sims@gmail.com>
2020-07-15 13:11:42 +0500

    Added Drivers's data in GetTrucks route.

--- routes/transportCompanyResponsibles/trucks/getTrucks.js
+++ routes/transportCompanyResponsibles/trucks/getTrucks.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const passport = require("../../../helpers/passportHelper");
 const Trucks = require("../../../models/trucks");
+const Drivers = require("../../../models/drivers");
+const DriverProfilePhotos = require("../../../models/DriverProfilePhotos");
 
 var router = express.Router();
 router.use(cors());
@@ -12,13 +14,36 @@ router.get("/getTrucks", (request, response) => {
         try {
             if (result.Message === "Transport company responsible found.") {
                 Trucks.findAll({
-                    attributes: ["TruckID", "DriverID", "TruckNumber", "Brand", "Model"],
+                    attributes: ["TruckID", "DriverID", "TruckNumber", "Brand", "Model", "PhotoURL"],
                     where: { TransportCompanyResponsibleID: result.TransportCompanyResponsible.TransportCompanyResponsibleID }
-                }).then(trucks => {
+                }).then(async trucks => {
                     if (trucks) {
+                        let mutableTrucks = [];
+                        let count = 0;
+
+                        for (let truck of trucks) {
+                            const driver = await Drivers.findOne({
+                                attributes: ["FirstName", "LastName"],
+                                where: { DriverID: truck.DriverID } 
+                            });
+
+                            const driverProfilePhoto = await DriverProfilePhotos.findOne({
+                                attributes: ["PhotoURL"],
+                                where: { DriverID: truck.DriverID } 
+                            });
+
+                            let mutableDriver = driver.dataValues;
+                            mutableDriver.PhotoURL = driverProfilePhoto ? driverProfilePhoto.PhotoURL : null;
+
+                            let mutableTruck = truck.dataValues;
+                            mutableTruck.Driver = mutableDriver;
+
+                            mutableTrucks[count++] = mutableTruck;
+                        }
+
                         response.json({
                             Message: "Trucks found.",
-                            Trucks: trucks
+                            Trucks: mutableTrucks
                         });
                     }
                     else {
